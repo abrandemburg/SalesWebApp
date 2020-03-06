a8d4c1dbf45b9e6763271ad50e99c63276bd9d12
Andre B Anjos <aanjos.brandemburg@gmail.com>
2020-03-06 12:24:26 -0300

    Adapting service to use MySQL and initial migration

--- SalesWebApp/Startup.cs
+++ SalesWebApp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesWebApp.Data;
 
 namespace SalesWebApp
 {
@@ -33,6 +35,10 @@ namespace SalesWebApp
 
 
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+        services.AddDbContext<SalesWebAppContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("SalesWebAppContext"), builder => 
+                  builder.MigrationsAssembly("SalesWebApp")));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
